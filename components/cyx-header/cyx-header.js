e683cc0302d0fde43375764dca1d3352b76145c5
uangwang <202119121729@stu.cdut.edu.cn>
2024-04-14 23:17:40 +0800

    Initial Commit

---
+++ components/cyx-header/cyx-header.js
@@ -0,0 +1,27 @@
+// components/cyx-header/cyx-header.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    clickPhone(){
+      wx.makePhoneCall({
+        phoneNumber: '19881165797',
+      })
+    }
+  }
+})
---
+++ components/cyx-news-item/cyx-news-item.js
@@ -0,0 +1,32 @@
+// components/cyx-news-item/cyx-news-item.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+        item: {
+            type: Object,
+            value: {
+                title: "默认测试标题",
+                author: "瞌睡王",
+                publish_date: 13,
+                view_count: 99
+            }
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
